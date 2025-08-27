90bdafb9b23cac97de4bfd615a703a176111768f
Yellow-Wolf-25 <25.yellow.wolf@gmail.com>
2025-08-28 02:53:18 +0530

    Basic

---
+++ Desktop/Arush Gupta/Solidity By Ex. Practice/Ether Wallet/src/etherWallet.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract EtherWallet {
+    address payable public owner;
+
+    enum Locked{
+        unlock,
+        lock
+    }
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    receive() external payable {}
+    
+
+    function withdraw(uint256 _amount) external {
+        require(Locked.unlock == Locked.unlock, "contract is locked");
+        require(msg.sender == owner, "caller is not owner");
+        payable(msg.sender).transfer(_amount);
+    }
+
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function lockContract() external {
+        require(msg.sender == owner, "caller is not owner");
+        Locked.lock;
+    }
+}
